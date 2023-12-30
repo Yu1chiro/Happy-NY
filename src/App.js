288808c5a850a7466a099f441e5f77500ada10a8
yu1chiro <takumiharu25@gmail.com>
2023-12-30 16:43:10 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import Particles from "react-particles"
+import { loadFireworksPreset } from "tsparticles-preset-fireworks"
+import { Typewriter } from "react-simple-typewriter"
+import { useState } from "react"
+import Countdown from "react-countdown"
+
+function App() {
+  const [newYearMessage, setNewYearMessage] = useState (["新年を待っています",])
+
+  const particlesInitialization = async (engine) => {
+    await loadFireworksPreset(engine)
+  }
+
+  function timeleft () {
+    const newYearDate = new Date ("January 1, 2024 00:00:00").getTime()
+    const nowDate = new Date().getTime()
+    const remainingTime = newYearDate - nowDate
+    return remainingTime 
+  }
+
+  return (
+    <>
+    <Particles 
+    init={particlesInitialization}
+    options={{ preset: "fireworks"}}
+    />
+    <div className="flex flex-col justify-center items-center min-h-screen gap-4">
+      <span className="text-white text-4xl font-bold px-4 z-50">
+        <Typewriter words={newYearMessage} 
+        loop={false}
+        cursorStyle={"👋"}
+        cursor
+        // Tambahkan typeSpeed jika perlu
+        />
+      </span>
+      <div className="z-50 text-white font-bold text-2xl">
+        <Countdown date={Date.now() + timeleft()} onComplete={() => setNewYearMessage (["明けまして", "おめでとうございます!✨"])} />
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
